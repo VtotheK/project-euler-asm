10f950b4034cf5363dd85f65fc56cf958df5c271
VtotheK <ville.keituri1@gmail.com>
2022-11-09 23:50:27 +0200

    print first number woopwoop

--- Problem-1/test.asm
+++ Problem-1/test.asm
@@ -2,12 +2,27 @@ section     .text
 global      _start
 
 _start:
-mov	eax, 0xFFEC
-mov	word[eax], 0xAB
 
-mov	eax, 1
+mov 	eax, 65
+call 	print_num
+call 	exit
+
+print_num:
+mov	ecx, 10
+div	ecx	;edx remainder, eax quotinent
+add	eax, 48
+mov 	[value], eax
+mov	ecx, value
+mov 	edx, 4
+mov	eax, 4
 mov	ebx, 1
+int	0x80 	;print value in ecx which is edx bytes length
+ret
+
+exit:
+mov 	eax, 1
 int	0x80
 
-section     .data
-addr1 	equ 0x1234
+section .data
+value	DW 0
+
